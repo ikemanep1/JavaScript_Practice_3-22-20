c9da84941c2ce13f7fca32f57e5e86e25486de24
Ike Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-03-22 11:18:57 -0700

    add additional content

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
     console.log("it works!");
     var myNum = 16;
-    console.log("your declared variable is " + myNum + "!")
+    console.log("your declared variable is " + myNum + "!");
+    myNum++;
+    console.log("your declared variable is now " + myNum + "!");
+    var num1 = 30;
+    var num2 = 60;
+    var remainder = num1 % num2;
+    console.log("the remainder of " + num1 + " and " + num2 + " is " + remainder + "!")
+    console.log("I am a \"double quoted\" string inside of \"double quotes \"");
 });
